d2639cb52d571f5dcf67a98f7fe98c5003b56f2a
posigamerX <ayomiposisewedo@gmail.com>
2023-02-03 11:58:23 +0100

    A simple calculator using react.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+
+import {useState} from 'react';
+import './index.css';
 
 function App() {
+  const [val, setVal] = useState("");
+
+  const backspace = () => {
+    try {
+      setVal(val.slice(0, -1))
+    }catch (error){
+
+    }
+  }
+  const calculate = ()=>{
+    try{
+      setVal(eval(val));
+    }catch(error){
+      setVal('Error')
+
+    }
+  }
+  const clear = () => {
+    setVal('')
+  }
+    
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    
+     <div className="container">
+      <div className="calculator">
+        <form >
+          <div className='display'>
+            <input type ='text' name ='display' value={val} onChange={(e) =>setVal(e.target.value)}/>
+          </div>
+          <div>
+            <input type='button' value='AC' onClick={() => clear()}/>
+            <input type='button' value='DE' onClick={() => backspace()} /> 
+            <input type='button' value='.' onClick={(e) => setVal(val +e.target.value)}/>
+            <input type='button' value='/' onClick={(e) => setVal(val +e.target.value)}/>  
+          </div>
+          <div>
+            <input type='button' value='7' onClick={(e) => setVal(val +e.target.value)}  />
+            <input type='button' value='8' onClick={(e) => setVal(val +e.target.value)} /> 
+            <input type='button' value='9' onClick={(e) => setVal(val +e.target.value)} />
+            <input type='button' value='*' onClick={(e) => setVal(val +e.target.value)}/>  
+          </div>
+          <div>
+            <input type='button' value='4' onClick={(e) => setVal(val +e.target.value)} />
+            <input type='button' value='5' onClick={(e) => setVal(val +e.target.value)} /> 
+            <input type='button' value='6' onClick={(e) => setVal(val +e.target.value)} />
+            <input type='button' value='-' onClick={(e) => setVal(val +e.target.value)} />  
+          </div>
+          <div>
+            <input type='button' value='1' onClick={(e) => setVal(val +e.target.value)} />
+            <input type='button' value='2' onClick={(e) => setVal(val +e.target.value)}/> 
+            <input type='button' value='3' onClick={(e) => setVal(val +e.target.value)}/>
+            <input type='button' value='+' onClick={(e) => setVal(val +e.target.value)}/>  
+          </div>
+          <div>
+            <input type='button' value='00' onClick={(e) => setVal(val +e.target.value)} />
+            <input type='button' value='0' onClick={(e) => setVal(val +e.target.value)} /> 
+            <input type='button' value='=' className='equal' onClick={() => calculate()}/> 
+          </div>
+        </form>
+      </div>
+     </div>
+  
+
   );
 }
 
